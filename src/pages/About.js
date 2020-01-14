a5e79d98405bdd3b2146ed0e1766cd6e18b5aa50
PanArtemG <artem253510@gmail.com>
2020-01-14 13:20:09 +0200

    created:to-do

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">Yours TO-DO list</h1>
+            <p className="lead">Version app: <strong>0.0.1</strong>.</p>
+            <p> Created using React, React Hooks,  Context, Animations.</p>
+            <time>Created: 15.01.2020</time>
+            <p>Developer: Artem Panasenko</p>
+        </div>
+    </div>
+);
